1dc47c5cb570e9f7c1a1fd1c43c133fe59ffd7ff
James McCutcheon <james_mccutcheon@hotmail.com>
2017-10-22 19:10:22 -0700

    Complete exercises

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,11 +1,15 @@
 ### Exercise 1 ###
 
 # Load the httr and jsonlite libraries for accessing data
-
+library(httr)
+library(jsonlite)
 
 ## For these questions, look at the API documentation to identify the appropriate endpoint and information.
 ## Then send GET() request to fetch the data, then extract the answer to the question
-
+response <- GET("http://data.unhcr.org/api/stats/time_series_years.json")
+body <- content(response,"text")
+years <- fromJSON(body)
+years
 # For what years does the API have statistical data?
 
 
